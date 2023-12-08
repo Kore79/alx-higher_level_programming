e7b381cd30fd957eec7dcaa029c44e66244d7cce
Kuku Oreoluwa <oorekuku@gmail.com>
2023-12-07 16:28:34 -0800

    Function to Covert to Uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-def is lower(c):
-    if ord(c) >= 97 and ord(c) <= 122:
-        return True
-    else:
-        return False
+def uppercase(str):
+    for ch in str:
+        if ord(ch) >= 97 and ord(ch) <= 122:
+            ch = chr(ord(ch) - 32)
+            print("{:s}".format(ch), end='')
+
+print('\n', end="")
